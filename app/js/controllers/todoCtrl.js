1abe2c8071c3c7841562ebb257a676c7359505c9
v.parkhomenko <parkhomenko.wd@gmail.com>
2016-07-19 18:21:15 +0300

    myTodo

--- app/js/controllers/todoCtrl.js
+++ app/js/controllers/todoCtrl.js
@@ -1,10 +1,30 @@
 'use strict';
 
-angular.module('myTodo').controller('TodoCtrl', function TodoCtrl($scope, $location, todoStorage) {
+angular.module('myTodo').controller('TodoCtrl', function TodoCtrl($scope, $location, todoStorage, $filter) {
     var todos = $scope.todos = todoStorage.get();
+
+    $scope.status = '';
+
     $scope.$watch('todos', function() {
         todoStorage.put(todos);
+        $scope.remainingCount = $filter('filter')(todos, { completed: false }).length;
+        $scope.completedCount = todos.length - $scope.remainingCount;
+        $scope.allChecked = !$scope.remainingCount;
     }, true);
+    
+    $scope.statusFilter = function(todo) {
+        if ($scope.status === 'active') {
+            return todo.completed === false;
+        } else if ($scope.status === 'completed') {
+            return todo.completed === true;
+        } else {
+            return todo;
+        }
+    };
+
+    $scope.changeStatus = function(currentStatus) {
+        $scope.status = currentStatus;
+    };
 
     $scope.addTodo = function() {
         if(!$scope.newTodo.length) {
@@ -34,21 +54,21 @@ angular.module('myTodo').controller('TodoCtrl', function TodoCtrl($scope, $locat
         todos.splice(todos.indexOf(todo), 1);
     };
 
-    $scope.clearCompletedTodos = function() {
-      $scope.todos = todos = todos.filter(function(value) {
-        return !value.completed;
-      });
-    };
-    
     $scope.markAll = function(completed) {
         todos.forEach(function(todo) {
             todo.completed = completed;
         });
     };
 
-/*    $scope.toggleCompleted = function ($index, completed) {//debugger;
+    $scope.toggleCompleted = function (todo, completed) {
         if (angular.isDefined(completed)) {
-            $index.completed = completed;
+            todo.completed = completed;
         }
-    };*/
+    };
+
+    $scope.clearCompletedTodos = function() {
+        $scope.todos = todos = todos.filter(function(value) {
+            return !value.completed;
+        });
+    };
 });
